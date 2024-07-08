1b1c92e4f59cd8f33a200005329c31ed29b619a6
Florian Witteler <github@flwi.de>
2024-07-08 11:50:54 +0200

    cleanup

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@
 import fs from "fs"
 import express from "express"
 import { WebSocketServer } from "ws"
-import { NetworkAdapterInterface, PeerId, Repo, RepoConfig } from "@automerge/automerge-repo"
+import { PeerId, Repo, RepoConfig } from "@automerge/automerge-repo"
 import { NodeWSServerAdapter } from "@automerge/automerge-repo-network-websocket"
 import { NodeFSStorageAdapter } from "@automerge/automerge-repo-storage-nodefs"
 import os from "os"
